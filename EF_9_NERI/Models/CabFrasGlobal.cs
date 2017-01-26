34e0ae8b5e50383c793f1660f5b6d7c3ccee0287
ivann3 <ivann3@RECEPCION>
2017-01-26 12:32:57 +0100

    Plantilla

---
+++ EF_9_NERI/Models/CabFrasGlobal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EF_9_NERI.Models
+{
+    public class CabFrasGlobal
+    {
+        public List<CabeceraFras> FacturasGlob = new List<CabeceraFras>();
+        public List<Pacientes> PacientesGlob = new List<Pacientes>();
+        public Pacientes paciente = new Pacientes();
+        public CabeceraFras factura = new CabeceraFras();
+        public decimal TOTAL { get; set; }
+    }
+}
